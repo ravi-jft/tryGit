fe650eebd53b25385d227242a76d17518e2eed1c
Ravi Kumar <ravikumaraggarwalabc@gmail.com>
2017-03-23 18:54:56 +0530

    Add Field in User

--- grails-app/domain/tryGit/User.groovy
+++ grails-app/domain/tryGit/User.groovy
@@ -3,9 +3,11 @@ package tryGit
 class User {
     String name
     String address
+    String email
 
     static constraints = {
      name blank: false
      address blank: false
+     email email: true
     }
 }
