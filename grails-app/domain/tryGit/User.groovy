2212e8716fa4da5508b108709df7f2c6b7c4b308
Ravi Kumar <ravikumaraggarwalabc@gmail.com>
2017-03-23 19:00:55 +0530

    Added some fields in domain

--- grails-app/domain/tryGit/User.groovy
+++ grails-app/domain/tryGit/User.groovy
@@ -4,10 +4,13 @@ class User {
     String name
     String address
     String email
+    Integer contact
+    String description
 
     static constraints = {
      name blank: false, maxSize: 255
      address blank: false, maxSize: 255
      email email: true
+     description blank: false, maxSize: 255
     }
 }
